4118199771329769e44dbcaf8fb7ea76ee00c9ba
Zetavblitz <147882829+Zetavblitz@users.noreply.github.com>
2025-03-09 14:09:18 -0500

    Assets and Primitive Navigation

--- scavenger_hunt/lib/main.dart
+++ scavenger_hunt/lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PFT Scavenger Hunt',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'PFT Scavenger Hunt'),
     );
   }
 }
@@ -30,9 +30,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Are you ready?',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const Panera(title: 'Starting Location: Panera Bread')),
+                );
+              },
+              child: Text(
+                'Begin',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Panera extends StatefulWidget {
+  const Panera({super.key, required this.title});
+  final String title;
+
+  @override
+  State<Panera> createState() => _PaneraState();
+}
+
+class _PaneraState extends State<Panera> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _begin() {
     setState(() {
       _counter++;
     });
@@ -49,20 +90,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Image(image: AssetImage('Panera.jpg')),
             Text(
-              '$_counter',
+              'Go to Panera Bread on the first floor of PFT.',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            TextButton(
+              onPressed: () => _begin,
+              child: Text(
+                'Continue',
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
